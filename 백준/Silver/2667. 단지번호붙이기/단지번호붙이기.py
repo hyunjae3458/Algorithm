d018f1b25a0d1a62a4faef35d5632483a50b60d8
hyunjae3458 <fredsonata@gmail.com>
2025-11-17 17:21:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,55 +1,59 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 N = int(input())
+
 grid = [
-    list(map(int,input()))
+    list(map(int,input().strip()))
     for _ in range(N)
 ]
 
-is_apt = [
+dp = [
+    [0] * N
+    for _ in range(N)
+]
+
+visited = [
     [False] * N
     for _ in range(N)
 ]
 
-apt_lst = []
-
-def init_apt():
-    for i in range(N):
-        for j in range(N):
-            if grid[i][j] == 1:
-                is_apt[i][j] = True
 
 def in_range(nx,ny):
     return nx >= 0 and nx < N and ny >= 0 and ny < N
 
-def bfs(x,y):
-    cnt = 1
+apt = []
+ 
+def bfs(x,y,apt_num):
     q = deque()
     q.append((x,y))
-    is_apt[x][y] = False
-
+    visited[x][y] = True
+    dp[x][y] = 1
     dxs = [1,0,-1,0]
     dys = [0,1,0,-1]
-    
+
     while q:
-        apt_x,apt_y = q.popleft()
-
+        cur_x,cur_y = q.popleft()
+        
         for dx,dy in zip(dxs,dys):
-            nx, ny = apt_x + dx, apt_y + dy
-            if in_range(nx,ny) and grid[nx][ny] and is_apt[nx][ny] == True:
-                q.append((nx,ny))
-                is_apt[nx][ny] = False
-                cnt += 1
-    return cnt
-
-init_apt()
-
+            nx,ny = cur_x + dx, cur_y + dy
+            if in_range(nx,ny) and grid[nx][ny] == 1:
+                if dp[nx][ny] == 0:
+                    dp[nx][ny] = 1
+                    visited[nx][ny] = True
+                    q.append((nx,ny))
+                    apt_num += 1
+    return apt_num
+    
 for i in range(N):
     for j in range(N):
-        if is_apt[i][j] == True:
-            apt_lst.append(bfs(i,j))
+        if grid[i][j] == 1 and not visited[i][j]:
+            apt.append(bfs(i,j,1))
 
-apt_lst.sort()
-print(len(apt_lst))
-for apt_num in apt_lst:
-    print(apt_num)
+apt.sort()
+print(len(apt))
+for i in apt:
+    print(i)
+
+
