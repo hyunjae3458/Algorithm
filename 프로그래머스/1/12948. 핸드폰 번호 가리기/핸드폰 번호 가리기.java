d8ffd530c078fc637279352d6c07e8ff09e071ca
hyunjae3458 <fredsonata@gmail.com>
2025-12-27 19:54:46 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        char[] arr = phone_number.toCharArray();
+        for(int i = 0; i < phone_number.length() - 4; i++){
+            arr[i] = '*';
+        }
+       
+        
+        
+        return new String(arr);
+    }
+}
